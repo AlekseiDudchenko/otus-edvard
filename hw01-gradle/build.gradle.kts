6ad5b5470e1df2b70e42ad83959b62e21dc3db3c
0563 <0563166@gmail.com>
2023-09-02 13:27:05 +0400

    module 01

---
+++ hw01-gradle/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("java")
+    id("com.github.johnrengelman.shadow") version "7.1.0"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.google.guava:guava:32.0.1-jre")
+}
+
+tasks.shadowJar {
+    archiveBaseName.set("my-app")
+    archiveVersion.set("1.0")
+    from(sourceSets.main.get().output)
+
+    manifest {
+        attributes["Main-Class"] = "org.example.HelloOtus"
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
